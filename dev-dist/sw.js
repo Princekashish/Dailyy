7048d7f2e15916c9607507c778d22453f189475c
Prince <princekashishbbu@gmail.com>
2026-05-17 01:03:48 +0530

    UI

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.2pq4k5sjis8"
+    "revision": "0.vlemhg35j6o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
